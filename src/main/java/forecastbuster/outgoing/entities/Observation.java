4b63840a42e907dca35e046afa03ba53e420beed
Kristjan Roosild <kristjan.r@gmail.com>
2012-12-30 22:09:32 +0200

    added date as a string so xstream could read it easily

--- src/main/java/forecastbuster/outgoing/entities/Observation.java
+++ src/main/java/forecastbuster/outgoing/entities/Observation.java
@@ -1,15 +1,33 @@
 package forecastbuster.outgoing.entities;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
 public class Observation {
-    Calendar date;
-    List<Station> stations;
+    private Calendar date;
+    private String dateString;
+    private List<Station> stations;
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    public void setDateString(String dateString) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            cal.setTime(sdf.parse(dateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        setDate(cal);
+    }
 
     public Observation(Calendar date, List<Station> stations) {
-        this.date = date;
-        this.stations = stations;
+        setDate(date);
+        setStations(stations);
     }
 
     public Calendar getDate() {
@@ -18,6 +36,9 @@ public class Observation {
 
     public void setDate(Calendar date) {
         this.date = date;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        String dateString = sdf.format(date.getTime());
+        this.dateString = dateString;
     }
 
     public List<Station> getStations() {
